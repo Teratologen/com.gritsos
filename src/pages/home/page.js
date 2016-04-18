ef4fa9d34666f03f34a5a964b6490b1f6e8f66cf
Sebastian Lindberg <seb.lindberg@gmail.com>
2016-04-18 16:57:31 +0200

    Added a banner to the home page

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./style.css";
 
+import Banner from "../../common/components/Banner/index.js";
+
 
 export default class HomePage extends React.Component {
   render() {
     return (
       <div className={styles.content}>
-        <h1>Home Page</h1>
-        <p className={styles.welcomeText}>Thanks for joining!</p>
+        <Banner>Gritsos</Banner>
+        <p>Welocome</p>
       </div>
     );
   }
