6ce5b0c74e91f35bf6963ae1f066b1c3de2eab01
Teratologen <johan.emil.jansson@gmail.com>
2016-04-19 19:15:47 +0200

    changes

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -8,8 +8,8 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div className={styles.content}>
-        <Banner>Gritsos</Banner>
-        <p>Welocome</p>
+
+        
       </div>
     );
   }
