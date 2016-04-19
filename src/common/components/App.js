6261ed938ac38716937f1feff92a7f17275c374d
Johan Jansson <JohanJansson@n139-p55.kthopen.kth.se>
2016-04-19 17:17:25 +0200

    Svg icon added to menu. Image added to banner. Multiple color changes

--- src/common/components/App.js
+++ src/common/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import Menu from './Menu.js';
-import MenuItem from './MenuItem.js';
+import Menu from './Menu';
+import MenuItem from './Menu/MenuItem.js';
 
 export default ({children}) => {
   return (
     <div id="container">
       <Menu name="name">
-        <MenuItem>Item 1</MenuItem>
-        <MenuItem>Item 2</MenuItem>
+        <MenuItem>Beer</MenuItem>
+        <MenuItem>Lifestyle</MenuItem>
+        <MenuItem>About us</MenuItem>
+        <MenuItem>Contact</MenuItem>
       </Menu>
       {children}
     </div>
--- src/common/components/Banner/index.js
+++ src/common/components/Banner/index.js
@@ -4,7 +4,6 @@ import style from './banner.css';
 export default class Banner extends React.Component {
   render() {
     const {children} = this.props;
-    
     return (
       <div className={style.banner}>
           <h1 className={style.bannerHead}>
